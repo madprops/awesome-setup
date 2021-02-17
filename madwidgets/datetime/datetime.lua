29a6eea18695dab1c6439d4f9526db71fe722109
Auric Vente <mprops@protonmail.com>
2021-02-17 09:23:34 -0600

    ampm

--- madwidgets/datetime/datetime.lua
+++ madwidgets/datetime/datetime.lua
@@ -5,7 +5,7 @@ local naughty = require("naughty")
 local datetime = {}
 
 function datetime.create(args)
-  local instance = awful.widget.textclock(" %a %b %d, %l:%M%P", 15)
+  local instance = awful.widget.textclock(" %a %b %d, %l:%M %P", 15)
 
   instance:connect_signal("button::press", function(a, b, c, button, mods)
     if button == 1 then
