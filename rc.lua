270e9031174a3165ec2dfee313b26b6ed029a054
Auric Vente <mprops@protonmail.com>
2021-03-01 11:18:35 -0600

    colormods

--- rc.lua
+++ rc.lua
@@ -45,14 +45,19 @@ local theme = beautiful.get()
 theme.font = "sans 11"
 beautiful.init(theme)
 
-local color_1 = "#3f3f3f"
 local color_white = "#ffffff"
+local font_color = "#a8a8a8"
+local bg_color = "#222222"
+local highlight_color = "#3f3f3f"
 
 beautiful.wibar_height = 25
-beautiful.tasklist_bg_focus = color_1
+beautiful.tasklist_fg_normal = font_color
+beautiful.tasklist_bg_normal = bg_color
 beautiful.tasklist_fg_focus = color_white
-beautiful.tasklist_bg_minimize = ""
-beautiful.taglist_bg_focus = color_1
+beautiful.tasklist_bg_focus = highlight_color
+beautiful.tasklist_fg_minimize = font_color
+beautiful.tasklist_bg_minimize = bg_color
+beautiful.taglist_bg_focus = highlight_color
 beautiful.notification_font = "Noto Sans 18px"
 beautiful.notification_icon_size = 100
 awful.mouse.snap.default_distance = 25
