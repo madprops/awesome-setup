e18479bd7fbc324baa147da5f9474adbe8ea080e
Auric Vente <mprops@protonmail.com>
2021-10-06 20:45:34 -0600

    Autostart pause mod

--- modules/autostart.lua
+++ modules/autostart.lua
@@ -1,5 +1,10 @@
 local awful = require("awful")
 
+function sleep(n)
+  os.execute("sleep " .. tonumber(n))
+end
+
+
 spawn("numlockx")
 spawn("xset m 0 0", false)
 spawn("xset r rate 220 40", false)
@@ -7,9 +12,13 @@ spawn("setxkbmap -option caps:none", false)
 
 shellspawn("killall -q plug.sh ; /home/yo/scripts/plug.sh")
 shellspawn("killall -q xbindkeys ; xbindkeys")
-shellspawn("pkill -f 'empris.py autopause'")
-shellspawn("pkill -f 'playerctl status --follow'")
-shellspawn("python3 /home/yo/code/empris/empris.py autopause")
 
 singlespawn("copyq")
 singlespawn("kdeconnect-indicator")
+
+shellspawn("pkill -f 'empris.py autopause'")
+shellspawn("pkill -f 'playerctl status --follow'")
+
+sleep(0.1)
+
+shellspawn("python3 /home/yo/code/empris/empris.py autopause")
