3274cba1ae1d0f1321d78abae271f59ba2540aa0
Auric Vente <mprops@protonmail.com>
2022-01-18 22:55:32 -0600

    Volume control change steps rb

--- madwidgets/volumecontrol/volumecontrol.lua
+++ madwidgets/volumecontrol/volumecontrol.lua
@@ -4,7 +4,7 @@ local utils = require("madwidgets/utils")
 
 local volumecontrol = {}
 volumecontrol.max_volume = 100
-volumecontrol.steps = 2
+volumecontrol.steps = 5
 
 local instances = {}
 local last_volume = 100
