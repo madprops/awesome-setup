bb575a057f6ca3531cf4645545fbb83bcbb0cea4
Auric Vente <mprops@protonmail.com>
2022-03-10 00:26:09 -0600

    Taglist rm buttons

--- modules/screens.lua
+++ modules/screens.lua
@@ -16,19 +16,8 @@ awful.screen.connect_for_each_screen(function(s)
       filter  = awful.widget.taglist.filter.all,
       buttons = {
         awful.button({ }, 1, function(t) t:view_only() end),
-        awful.button({ modkey }, 1, function(t)
-                                        if client.focus then
-                                            client.focus:move_to_tag(t)
-                                        end
-                                    end),
-        awful.button({ }, 3, awful.tag.viewtoggle),
-        awful.button({ modkey }, 3, function(t)
-                                        if client.focus then
-                                            client.focus:toggle_tag(t)
-                                        end
-                                    end),
         awful.button({ }, 4, function(t) prev_tag() end),
-        awful.button({ }, 5, function(t) next_tag() end),
+        awful.button({ }, 5, function(t) next_tag() end)
       }
   }  
 
