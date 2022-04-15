34ea7880b483898e2a1a7ee7ce9f302a22392415
Auric Vente <mprops@protonmail.com>
2022-04-15 17:50:58 -0600

    xset mods

--- modules/autostart.lua
+++ modules/autostart.lua
@@ -1,8 +1,13 @@
 local awful = require("awful")
 
 spawn("numlockx")
+
 spawn("xset m 0 0", false)
 spawn("xset r rate 220 40", false)
+spawn("xset s off", false)
+spawn("xset -dpms", false)
+spawn("xset s noblank", false)
+
 spawn("setxkbmap -option caps:none", false)
 
 shellspawn("pkill -f 'clipton'")
