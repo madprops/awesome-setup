9b2d3b0d5864cf68675adc6ab153923befac3fcd
Auric Vente <mprops@protonmail.com>
2022-07-06 02:41:05 -0600

    Mods

--- modules/autostart.lua
+++ modules/autostart.lua
@@ -1,5 +1,6 @@
 local awful = require("awful")
 
+-- X Settings
 spawn("numlockx")
 spawn("xset m 0 0", false)
 spawn("xset r rate 220 40", false)
@@ -7,3 +8,6 @@ spawn("xset s off", false)
 spawn("xset -dpms", false)
 spawn("xset s noblank", false)
 spawn("setxkbmap -option caps:none", false)
+
+-- Clipboard Manager
+spawn("systemctl --user start clipton", false)
