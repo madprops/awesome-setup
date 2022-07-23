91684cb149c111857d889639aeec1d1aa7a06ae9
Auric Vente <mprops@protonmail.com>
2022-07-23 08:57:14 -0600

    Mod

--- modules/notifications.lua
+++ modules/notifications.lua
@@ -36,3 +36,11 @@ ruled.notification.connect_signal("request::rules", function()
     }
   }
 end)
+
+naughty.connect_signal("request::display_error", function(message, startup)
+  naughty.notification {
+    urgency = "critical",
+    title   = "Oops, an error happened"..(startup and " during startup!" or "!"),
+    message = message
+  }
+end)
