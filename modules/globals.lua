06103f008591f868147ae1c6459fc53fa4dcc1bf
Auric Vente <mprops@protonmail.com>
2022-08-29 07:30:01 -0600

    Mod

--- modules/globals.lua
+++ modules/globals.lua
@@ -1,7 +1,7 @@
 Globals = {}
 
 Globals.nicegreen = "#6FE2C8"
-Globals.niceblue = "#1880EB"
+Globals.niceblue = "#25AEF3"
 Globals.nicered = "#E9006A"
 Globals.nicedark = "#445666"
 Globals.flower = "❇"
