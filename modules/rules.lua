a71edaa7bc183bf1e6176dc9547cab5056740b02
Auric Vente <mprops@protonmail.com>
2022-12-12 17:36:05 -0600

    Mods

--- modules/rules.lua
+++ modules/rules.lua
@@ -170,19 +170,19 @@ function Rules.check_title_rules(c)
 
   if Utils.startswith(c.name, "[ff_tile1]") then
     c.width = Utils.width_factor(0.5)
-    c.height = Utils.height_factor(0.5)
+    c.height = Utils.height_factor(0.6)
     c.xindex = 1
     c.maximized = false
     xindex = 1
-    Utils.snap(c, "corner", awful.placement.top_left)
+    awful.placement.top_left(c, {honor_workarea = true})
   end
 
   if Utils.startswith(c.name, "[ff_tile2]") then
     c.width = Utils.width_factor(0.5)
-    c.height = Utils.height_factor(0.5)
+    c.height = Utils.height_factor(0.4)
     c.xindex = 2
     c.maximized = false
-    Utils.snap(c, "corner", awful.placement.bottom_left)
+    awful.placement.bottom_left(c, {honor_workarea = true})
   end
 end
 
