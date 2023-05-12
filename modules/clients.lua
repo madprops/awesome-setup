61be6eced57cb7af1c682956c8c9eb31158eabba
Auric Vente <mprops@protonmail.com>
2023-05-11 22:59:40 -0600

    Mods

--- modules/clients.lua
+++ modules/clients.lua
@@ -1,5 +1,9 @@
 client.connect_signal("manage", function(c)
   Rules.check_title_rules(c)
+
+  if c.xutil then
+    c.hidden = true
+  end
 end)
 
 client.connect_signal("property::name", function(c)
