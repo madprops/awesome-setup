34341042533c3d05300035eab1b93ee1b813c6e3
Auric Vente <mprops@protonmail.com>
2023-08-24 14:09:12 -0600

    Scripts

---
+++ scripts/commands.rb
@@ -0,0 +1,21 @@
+#!/usr/bin/env ruby
+require "open3"
+
+def pick_cmd(prompt, data)
+  cmd = "rofi -dmenu -p '#{prompt}' -me-select-entry '' -me-accept-entry 'MousePrimary' -i"
+  stdin, stdout, stderr, wait_thr = Open3.popen3(cmd)
+  stdin.puts(data.split("\n"))
+  stdin.close
+  return stdout.read.strip
+end
+
+path = File.join(__dir__, "data/commands.data")
+data = File.read(path).strip
+cmd = pick_cmd("Select Command", data)
+
+if cmd.empty?
+  return
+end
+
+system("xdotool type '#{cmd}'")
+system("xdotool key Return")
---
+++ scripts/save_command.rb
@@ -0,0 +1,36 @@
+#!/usr/bin/env ruby
+if ARGV[0].nil?
+  return
+end
+
+cmd = ARGV[0].strip
+
+if cmd.empty?
+  return
+end
+
+xcmds_equals = ["cd", "ls", "lq", "z", "br", "o"]
+xcmds_starts = ["cd", "z"]
+
+if xcmds_equals.any? { |x| cmd == x }
+  return
+end
+
+if xcmds_starts.any? { |x| cmd.start_with?(x) }
+  return
+end
+
+path = File.join(__dir__, "data/commands.data")
+max_items = 500
+lines = []
+
+File.open(path, "a+") do |file|
+  lines = file.readlines
+end
+
+lines.prepend(cmd)
+content = lines.map(&:chomp).uniq.take(max_items).join("\n")
+
+File.open(path, "w") do |file|
+  file.puts(content)
+end
