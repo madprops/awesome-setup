3af1711fba0f95c66a3f12114053819ae49d6b21
Auric Vente <mprops@protonmail.com>
2023-08-24 14:12:01 -0600

    Scripts

--- scripts/timer.rb
+++ scripts/timer.rb
@@ -1,9 +1,6 @@
 #!/usr/bin/env ruby
 require "open3"
 
-
-
-# Get input information using rofi
 def get_input(prompt, data)
   cmd = "rofi -dmenu -p '#{prompt}' -i"
   stdin, stdout, stderr, wait_thr = Open3.popen3(cmd)
@@ -12,52 +9,44 @@ def get_input(prompt, data)
   return stdout.read.strip
 end
 
-# Main function
-def main()
-  path = File.join(__dir__, "data/timer.data")
-  data = File.read(path).strip
+path = File.join(__dir__, "data/timer.data")
+data = File.read(path).strip
 
-  if ARGV.length >= 2
-    title = ARGV[0]
-    time = ARGV[1..-1].join(" ")
-  else
+if ARGV.length >= 2
+  title = ARGV[0]
+  time = ARGV[1..-1].join(" ")
+else
 
-    title = get_input("Enter Title", data)
+  title = get_input("Enter Title", data)
 
-    if title == ""
-      exit
-    end
-
-    times = "5m\n10m\n15m\n20m\n30m\n1h\n2h"
-    time = get_input("Enter Time", times)
-
-    if time == ""
-      exit
-    end
+  if title == ""
+    exit
   end
 
-  n = time.gsub(/[^0-9]/, "").strip.to_i
-  u = time.gsub(/[0-9]/, "").strip
+  times = "5m\n10m\n15m\n20m\n30m\n1h\n2h"
+  time = get_input("Enter Time", times)
 
-  if u == "m" or u.include?("min")
-    t = n
-  elsif u == "s" or u.include?("sec")
-    t = n / 60.0
-  elsif u == "h" or u.include?("hour")
-    t = n * 60.0
-  else
-    t = n
+  if time == ""
+    exit
   end
-
-  lines = data.split("\n")
-  lines.delete_if {|x| x == title}
-  lines.unshift(title)
-  File.write(path, lines.join("\n"))
-
-  `awesome-client 'Utils.timer("#{title}", #{t})'`
 end
 
-# Start here
-if __FILE__ == $0
-  main()
+n = time.gsub(/[^0-9]/, "").strip.to_i
+u = time.gsub(/[0-9]/, "").strip
+
+if u == "m" or u.include?("min")
+  t = n
+elsif u == "s" or u.include?("sec")
+  t = n / 60.0
+elsif u == "h" or u.include?("hour")
+  t = n * 60.0
+else
+  t = n
 end
+
+lines = data.split("\n")
+lines.delete_if {|x| x == title}
+lines.unshift(title)
+File.write(path, lines.join("\n"))
+
+`awesome-client 'Utils.timer("#{title}", #{t})'`
