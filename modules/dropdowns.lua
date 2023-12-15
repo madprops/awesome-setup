543fd06cfd7743104d73c901b8349a9f34873957
Auric Vente <mprops@protonmail.com>
2023-12-15 05:45:08 -0600

    Dropdowns

--- modules/dropdowns.lua
+++ modules/dropdowns.lua
@@ -24,66 +24,66 @@ function Dropdowns.toggle(what)
 	end
   end
 
-  function Dropdowns.show(what)
-	Dropdowns.hide_others(what)
-	local t = Utils.mytag()
-	local max
+function Dropdowns.show(what)
+Dropdowns.hide_others(what)
+local t = Utils.mytag()
+local max
 
-	for _, c in ipairs(client.get()) do
-	  if c["x" .. what] then
-		c:move_to_tag(t)
-		c.hidden = false
-		c:raise()
+for _, c in ipairs(client.get()) do
+	if c["x" .. what] then
+	c:move_to_tag(t)
+	c.hidden = false
+	c:raise()
 
-		if not Dropdowns[what .. "_on"] then
-		  Rules.reset_rules(c)
-		else
-		  if c.maximized then
-			max = c
-		  end
+	if not Dropdowns[what .. "_on"] then
+		Rules.reset_rules(c)
+	else
+		if c.maximized then
+		max = c
 		end
-	  end
 	end
-
-	if max ~= nil then
-	  Utils.focus(max)
 	end
+end
 
-	Dropdowns[what .. "_on"] = true
-	Dropdowns[what .. "_screen"] = Utils.myscreen()
-	Dropdowns[what .. "_tag"] = Utils.mytag()
-  end
+if max ~= nil then
+	Utils.focus(max)
+end
 
-  function Dropdowns.hide(what)
-	for _, c in ipairs(client.get()) do
-	  if c["x" .. what] then
-		c.hidden = true
-	  end
+Dropdowns[what .. "_on"] = true
+Dropdowns[what .. "_screen"] = Utils.myscreen()
+Dropdowns[what .. "_tag"] = Utils.mytag()
+end
+
+function Dropdowns.hide(what)
+for _, c in ipairs(client.get()) do
+	if c["x" .. what] then
+	c.hidden = true
 	end
+end
 
-	Dropdowns[what .. "_on"] = false
-  end
+Dropdowns[what .. "_on"] = false
+end
 
-  function Dropdowns.hide_others(what)
-	local dropdowns = {"util_screen", "chat_gpt"}
+function Dropdowns.hide_others(what)
+local dropdowns = {"util_screen", "chat_gpt"}
 
-	for index, dropdown in ipairs(dropdowns) do
-	  if dropdown ~= what then
-		if Dropdowns[dropdown .. "_on"] then
-		  Dropdowns.hide(dropdown)
-		end
-	  end
+for index, dropdown in ipairs(dropdowns) do
+	if dropdown ~= what then
+	if Dropdowns[dropdown .. "_on"] then
+		Dropdowns.hide(dropdown)
 	end
-  end
-
-  function Dropdowns.check()
-	local dropdowns = {"util_screen", "chat_gpt"}
-
-	for index, dropdown in ipairs(dropdowns) do
-	  if Dropdowns[dropdown .. "_on"] then
-		if Dropdowns[dropdown .. "_screen"] == Utils.myscreen() then
-		  Dropdowns.hide(dropdown)
-		end
-	  end
 	end
-  end
+end
+end
+
+function Dropdowns.check()
+local dropdowns = {"util_screen", "chat_gpt"}
+
+for index, dropdown in ipairs(dropdowns) do
+	if Dropdowns[dropdown .. "_on"] then
+	if Dropdowns[dropdown .. "_screen"] == Utils.myscreen() then
+		Dropdowns.hide(dropdown)
+	end
+	end
+end
+end
