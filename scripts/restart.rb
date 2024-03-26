7c4c52e4905ca8b546b3c3ece162a73bd9789444
Auric Vente <mprops@protonmail.com>
2024-03-26 03:51:15 -0600

    Mods

--- scripts/restart.rb
+++ scripts/restart.rb
@@ -1,17 +1,25 @@
 #!/usr/bin/env ruby
+# Restart/redo last command
+
+# Delay to ensure things work
 delay = 0.125
 
-sleep(delay)
+# Trigger click to focus correct location
+# For instance a specific terminal split
 `xdotool click 1`
 
+# Stop process if any
 sleep(delay)
 `xdotool key Ctrl+c`
 
+# I needed this for some reason
 sleep(delay)
 `xdotool key Ctrl+u`
 
+# Seek last command
 sleep(delay)
 `xdotool key Up`
 
+# Execute command
 sleep(delay)
 `xdotool key Return`
